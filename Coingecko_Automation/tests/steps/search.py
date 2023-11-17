c74417de68a3048e56c0887be1cb0429d0951e03
Maciej Antosiak <maciej.jerzy.antosiak@gmail.com>
2023-11-18 00:34:56 +0100

    adding sleeps for debugging

--- Coingecko_Automation/tests/steps/search.py
+++ Coingecko_Automation/tests/steps/search.py
@@ -1,5 +1,5 @@
 from behave import *
-
+import time
 from Coingecko_Automation.tests.pages import coin_page
 from Coingecko_Automation.tests.pages.coin_page import CoinPage
 from Coingecko_Automation.tests.pages.home_page import HomePage
@@ -8,18 +8,20 @@ from Coingecko_Automation.tests.pages.home_page import HomePage
 @given('I am on home page')
 def step_impl(context):
     context.home_page = HomePage(context.driver)
-
+    time.sleep(10)
     assert 'https://www.coingecko.com/' == context.home_page.url
     assert 'Cryptocurrency Prices, Charts, and Crypto Market Cap | CoinGecko' == context.home_page.title
 
 
 @when('I search for "{coin}"')
 def step_impl(context, coin):
+    time.sleep(10)
     context.home_page.search(coin)
 
 
 @then('page should display "{coin}" page')
 def step_impl(context, coin):
+    time.sleep(10)
     context.coin_page = CoinPage(context.driver)
     print(context.coin_page.url)
     assert f'https://www.coingecko.com/en/coins/{coin.lower()}' == context.coin_page.url
