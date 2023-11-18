791f4910ba61ba5df73fa53d8c9b1f3373615128
Maciej Antosiak <maciej.jerzy.antosiak@gmail.com>
2023-11-18 01:25:15 +0100

    adding sleeps for debugging

--- Coingecko_Automation/tests/environment.py
+++ Coingecko_Automation/tests/environment.py
@@ -4,10 +4,13 @@ from selenium.webdriver.chrome.options import Options
 
 def before_scenario(context, scenario):
     options = Options()
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
     options.add_argument('--window-size=1920,1080')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-extensions')
-
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--allow-running-insecure-content')
+    options.add_argument(f'user-agent={user_agent}')
     driver = webdriver.Chrome(options=options)
     #driver.maximize_window()
     driver.get('https://www.coingecko.com/')
