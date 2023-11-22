cef03bbafd64776cbcbe1c4926737d7165ea28da
maciejjerzyantosiak <111027778+maciejjerzyantosiak@users.noreply.github.com>
2023-11-22 01:40:28 +0100

    removing print statements

--- CoingeckoAutomation/Web/tests/steps/search.py
+++ CoingeckoAutomation/Web/tests/steps/search.py
@@ -6,8 +6,6 @@ from CoingeckoAutomation.Web.tests.pages.home_page import HomePage
 @given('I am on home page')
 def step_impl(context):
     context.home_page = HomePage(context.driver)
-
-    print('Starting')
     assert 'https://www.coingecko.com/' == context.home_page.url
     assert 'Cryptocurrency Prices, Charts, and Crypto Market Cap | CoinGecko' == context.home_page.title
 
@@ -20,7 +18,6 @@ def step_impl(context, coin):
 @then('page should display "{coin}" page')
 def step_impl(context, coin):
     context.coin_page = CoinPage(context.driver)
-    print(context.coin_page.url)
     coin = coin.lower().split(' ')[0]
     assert f'https://www.coingecko.com/en/coins/{coin.lower()}' == context.coin_page.url
     assert coin == context.coin_page.coin_name.text.lower()
